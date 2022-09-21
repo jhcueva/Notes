8680ddc361359dedef5a34eafb7d5f20850a4d5a
jhcueva <humbertocueva97@gmail.com>
2022-09-21 16:40:28 -0500

    [BUILD] Local docker compose fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,14 @@ import svgr from 'vite-plugin-svgr'; // make sure to import it
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    proxy: {
+      "/api": {
+        target: "http://192.168.96.5:8000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
     host: '0.0.0.0',
     port: 3000,
   },
--- project/api/views.py
+++ project/api/views.py
@@ -1,14 +1,13 @@
-
-# Django REST Framework
+from django.http import response
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
-# Models
+from rest_framework.serializers import Serializer
 from .models import Note
-
-# Serializers
 from .serializers import NoteSerializer
 from api import serializers
+from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
+# Create your views here.
 
 
 @api_view(['GET'])
@@ -46,44 +45,62 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-
     return Response(routes)
 
 
+# /notes GET
+# /notes POST
+# /notes/<id> GET
+# /notes/<id> PUT
+# /notes/<id> DELETE
+
 @api_view(['GET', 'POST'])
 def getNotes(request):
+
     if request.method == 'GET':
-        notes = Note.objects.all()
-        serializer = NoteSerializer(notes, many=True)
-        return Response(serializer.data)
+        return getNotesList(request)
 
     if request.method == 'POST':
-        data = request.data
-        note = Note.objects.create(
-            body=data['body']
-        )
-        serializers = NoteSerializer(note, many=True)
-        return Response(serializers.data)
+        return createNote(request)
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def getNote(request, pk):
+
     if request.method == 'GET':
-        notes = Note.objects.get(id=pk)
-        serializer = NoteSerializer(notes, many=False)
-        return Response(serializer.data)
+        return getNoteDetail(request, pk)
 
     if request.method == 'PUT':
-        data = request.data
-        note = Note.objects.get(id=pk)
-        serializer = NoteSerializer(instance=note, data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-
-        return Response(serializer.data)
+        return updateNote(request, pk)
 
     if request.method == 'DELETE':
-        note = Note.objects.get(id=pk)
-        note.delete()
-        return Response('Note was deleted')
+        return deleteNote(request, pk)
+
+
+# @api_view(['POST'])
+# def createNote(request):
+#     data = request.data
+#     note = Note.objects.create(
+#         body=data['body']
+#     )
+#     serializer = NoteSerializer(note, many=False)
+#     return Response(serializer.data)
+
+
+# @api_view(['PUT'])
+# def updateNote(request, pk):
+#     data = request.data
+#     note = Note.objects.get(id=pk)
+#     serializer = NoteSerializer(instance=note, data=data)
+
+#     if serializer.is_valid():
+#         serializer.save()
+
+#     return Response(serializer.data)
+
+
+# @api_view(['DELETE'])
+# def deleteNote(request, pk):
+#     note = Note.objects.get(id=pk)
+#     note.delete()
+#     return Response('Note was deleted!')
